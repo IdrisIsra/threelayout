a8f3089cab8dcd6ccb8f06470a587b24a3370792
IdrisIsra <israpilov.idris@gmail.com>
2022-10-31 20:27:47 +0300

    added a working page layout with drei scroll controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as THREE from "three";
+import { useRef, useState, useEffect } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  useIntersect,
+  Image,
+  ScrollControls,
+  Scroll,
+  Html,
+} from "@react-three/drei";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Item({
+  url,
+  scale,
+  ...props
+}: {
+  url: string;
+  scale: any;
+  position: any;
+}) {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <group {...props}>
+      <Image scale={scale} url={url} />
+    </group>
+  );
 }
 
-export default App
+function Items() {
+  const { width: w, height: h } = useThree((state: any) => state.viewport);
+  return (
+    <Scroll>
+      <Item
+        url="/vite.svg"
+        scale={[w / 3, w / 3, 1]}
+        position={[-w / 6, 0, 0]}
+      />
+      <Item url="/vite.svg" scale={[2, w / 3, 1]} position={[w / 30, -h, 0]} />
+      <Item
+        url="/vite.svg"
+        scale={[w / 3, w / 5, 1]}
+        position={[-w / 4, -h * 1, 0]}
+      />
+      <Item
+        url="/vite.svg"
+        scale={[w / 5, w / 5, 1]}
+        position={[w / 4, -h * 1.2, 0]}
+      />
+      <Item
+        url="/vite.svg"
+        scale={[w / 5, w / 5, 1]}
+        position={[w / 10, -h * 1.75, 0]}
+      />
+      <Item
+        url="/vite.svg"
+        scale={[w / 3, w / 3, 1]}
+        position={[-w / 4, -h * 2, 0]}
+      />
+      <Item
+        url="/vite.svg"
+        scale={[w / 3, w / 5, 1]}
+        position={[-w / 4, -h * 2.6, 0]}
+      />
+      <Item
+        url="/vite.svg"
+        scale={[w / 2, w / 2, 1]}
+        position={[w / 4, -h * 3.1, 0]}
+      />
+      <Item
+        url="/vite.svg"
+        scale={[w / 2.5, w / 2, 1]}
+        position={[-w / 6, -h * 4.1, 0]}
+      />
+    </Scroll>
+  );
+}
+
+function App() {
+  return (
+    <Canvas
+      orthographic
+      camera={{ zoom: 80 }}
+      gl={{ alpha: false, antialias: false, stencil: false, depth: false }}
+      dpr={[1, 1.5]}
+    >
+      <color attach="background" args={["#f0f0f0"]} />
+      <ScrollControls damping={6} pages={5}>
+        <Items />
+        <Scroll html style={{ width: "100%" }}>
+          <h1
+            style={{
+              position: "absolute",
+              top: `100vh`,
+              right: "20vw",
+              fontSize: "25em",
+              transform: `translate3d(0,-100%,0)`,
+            }}
+          >
+            all
+          </h1>
+          <h1 style={{ position: "absolute", top: "180vh", left: "10vw" }}>
+            hail
+          </h1>
+          <h1 style={{ position: "absolute", top: "260vh", right: "10vw" }}>
+            thee,
+          </h1>
+          <h1 style={{ position: "absolute", top: "350vh", left: "10vw" }}>
+            thoth
+          </h1>
+          <h1 style={{ position: "absolute", top: "450vh", right: "10vw" }}>
+            her
+            <br />
+            mes.
+          </h1>
+        </Scroll>
+      </ScrollControls>
+    </Canvas>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { Suspense } from "react";
+import "./styles.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function Overlay() {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        pointerEvents: "none",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <a
+        href="https://pmnd.rs/"
+        style={{ position: "absolute", top: 40, left: 90, fontSize: "13px" }}
+      >
+        Testing pmndrs examples
+      </a>
+      <div
+        style={{ position: "absolute", top: 40, right: 40, fontSize: "13px" }}
+      >
+        31/10/2022
+      </div>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <>
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+    <Overlay />
+  </>
+);
